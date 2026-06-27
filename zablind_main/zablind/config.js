12314060ffc225dbff9139c6f51dbe1d2dc62b7b
oceanondawave <minh.ngntri@gmail.com>
2026-06-27 16:43:35 +0700

    chore: bump version to 2.0.1

--- zablind_main/zablind/config.js
+++ zablind_main/zablind/config.js
@@ -2,7 +2,7 @@
 // Centralized Zablind Settings
 
 module.exports = {
-  version: "2.0",
+  version: "2.0.1",
   year: "2026",
   // Developer Settings (Set to false in production)
   enableDevTools: false,         // Automatically open detached DevTools window on Zalo startup
